4ff5c7e9c283a5d4e9ca5636eb224cefad391152
chenwj <731028571@qq.com>
2017-03-17 10:28:35 +0800

    shiny-scroll

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-export const shinyScroll = {}
+import throttle from 'lodash.throttle'
+const triggers = ["scroll"]
+let watch = () => {
+    console.log(this)
+}
+let check = function (throttleNum) {
+    return throttle(watch, throttleNum)
+}
+export const shinyScroll = (ele, fn, offset = 10, throttleNum = 200) => {
+    triggers.forEach(event => {
+        ele.addEventListener(event, check(throttleNum))
+    })
+}
