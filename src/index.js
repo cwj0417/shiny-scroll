6aa01f370d3bae5f7052c3ccf1af56451da523f9
chenwj <731028571@qq.com>
2017-03-21 01:06:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@
  */
 import throttle from "lodash.throttle"
 const triggers = ["scroll"]
-let watch = function () {
-    console.log(this)
-}
-let check = function (throttleNum) {
-    return throttle(watch, throttleNum)
+let check = function (throttleNum, offset, fn) {
+    return throttle(function () {
+        if (this.scrollHeight - offset < this.scrollTop + this.offsetHeight) {
+            fn()
+        }
+        ,
+        throttleNum
+        )
 }
 export const shinyScroll = (ele, fn, offset = 10, throttleNum = 200) => {
     triggers.forEach(event => {
-        ele.addEventListener(event, check(throttleNum))
+        ele.addEventListener(event, check(throttleNum, offset, fn))
     })
 }
