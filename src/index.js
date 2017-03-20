76a765d754e0a05fcd9ce7801055dde28c0a5dd3
chenwj <731028571@qq.com>
2017-03-21 01:08:49 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ let check = function (throttleNum, offset, fn) {
     return throttle(function () {
         if (this.scrollHeight - offset < this.scrollTop + this.offsetHeight) {
             fn()
-        }, throttleNum)
+        }
+    }, throttleNum)
 }
 export const shinyScroll = (ele, fn, offset = 10, throttleNum = 200) => {
     triggers.forEach(event => {
